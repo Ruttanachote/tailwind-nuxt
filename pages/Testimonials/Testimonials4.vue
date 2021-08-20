048a2d8fda6e1db6ed210b70fcca237040369145
Ruttanachot <nui-jin@hotmail.com>
2021-08-20 19:13:20 +0700

    Up Template

---
+++ pages/Testimonials/Testimonials4.vue
@@ -0,0 +1,27 @@
+<template>
+ 
+<!-- Section 1 -->
+<section class="relative py-20 overflow-hidden bg-gray-900 cursor-pointer sm:py-24 md:py-32">
+    <div class="relative max-w-5xl px-16 mx-auto xl:px-0">
+        <svg class="absolute top-0 left-0 hidden w-32 h-32 -mt-3 -ml-16 text-purple-300 opacity-50 xl:block" stroke="currentColor" fill="none" viewBox="0 0 144 144"><path stroke-width="2" d="M41.485 15C17.753 31.753 1 59.208 1 89.455c0 24.664 14.891 39.09 32.109 39.09 16.287 0 28.386-13.03 28.386-28.387 0-15.356-10.703-26.524-24.663-26.524-2.792 0-6.515.465-7.446.93 2.327-15.821 17.218-34.435 32.11-43.742L41.485 15zm80.04 0c-23.268 16.753-40.02 44.208-40.02 74.455 0 24.664 14.891 39.09 32.109 39.09 15.822 0 28.386-13.03 28.386-28.387 0-15.356-11.168-26.524-25.129-26.524-2.792 0-6.049.465-6.98.93 2.327-15.821 16.753-34.435 31.644-43.742L121.525 15z"></path></svg>
+        <div class="relative xl:pl-32 lg:flex lg:items-center">
+            <div class="relative">
+                <blockquote class="relative">
+                    <div class="font-serif text-xl font-medium leading-9 text-gray-100 md:text-2xl">
+                        <p>Our sales increased 10x after integrating these tools. A must-have for anyone who wants to take their business to the next level!</p>
+                    </div>
+                    <div class="mt-4">
+                        <div class="flex items-center">
+                            <div class="flex-shrink-0"><img class="object-cover w-12 h-12 mr-4 rounded-full" src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;faces=1&amp;faceindex=1&amp;facepad=2.5&amp;w=500&amp;h=500&amp;q=80" alt=""> </div>
+                            <div class="ml-4 lg:ml-0">
+                                <div class="text-base font-medium leading-6 text-purple-500">Bobby Jones - Sr. Developer, SomeCompany</div>
+                            </div>
+                        </div>
+                    </div>
+                </blockquote>
+            </div>
+        </div>
+    </div>
+</section>
+
+</template>
